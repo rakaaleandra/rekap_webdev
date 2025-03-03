4417cb1c15725691e34294df09266e8cdedacfce
rakaaleandra <raka.aleandra@gmail.com>
2025-03-03 11:47:14 +0700

    penambahan javascript

---
+++ mini_project/scripts/myscripts.js
@@ -0,0 +1,17 @@
+function myFunction2(){
+    document.getElementById('demo').innerText = Date();
+}
+
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+console.log(z);
+
+function myFunction1(){
+    alert(z);
+}
+
+let string1 = "2" + 1 + 2, string2 = 2 + 1 + "2";
+console.log(string1);
+console.log(string2);
