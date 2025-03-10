a3a283f1574c45ea2e1be4c954736415f1134d49
rakaaleandra <raka.aleandra@gmail.com>
2025-03-10 20:32:27 +0700

    pengeditan

--- mini_project/scripts/myscripts.js
+++ mini_project/scripts/myscripts.js
@@ -15,3 +15,26 @@ function myFunction1(){
 let string1 = "2" + 1 + 2, string2 = 2 + 1 + "2";
 console.log(string1);
 console.log(string2);
+
+function myfunction3(){
+    let x = [
+        "John",
+        "Doe",
+        30
+    ];
+    let text = "";
+    x.forEach(myFunction4);
+    function myFunction4(value){
+        text += value + "<br>";
+    }
+    document.getElementById("demo2").innerHTML = text;
+}
+
+let person = {
+    fullName : function(firstName, lastName){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+let rasen = person.fullName("Rasen", "dria");
+document.getElementById("demo3").innerHTML = rasen;
